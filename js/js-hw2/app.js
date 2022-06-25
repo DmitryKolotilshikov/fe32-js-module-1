1fbfe1a911ba8ad7deabff0f751344a833d4a0bb
Shovgur <s.showgur@yandex.by>
2022-06-25 19:26:57 +0300

    added task 2

---
+++ js/js-hw2/app.js
@@ -0,0 +1,17 @@
+let x = 20
+let y = 58
+let z = 42
+
+console.log (x +y+z)
+
+let a = 60
+let b = 60
+let c = 24
+let d = 364
+
+let myAgeInDay= d*22
+let myAgeInHours= myAgeInDay*24
+let myAgeInMinute= myAgeInHours*60
+let myAgeInSeconds= myAgeInHours*60
+
+console.log(myAgeInSeconds)
