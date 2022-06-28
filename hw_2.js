479f51ec78b14e703e380c68b87500b963c73af0
verashibko <vera_shibko@mail.ru>
2022-06-28 16:49:24 +0400

    init(hw_2): start course Javascript Basic

---
+++ hw_2.js
@@ -0,0 +1,42 @@
+// TASK 1
+let x = 20
+let y = 58
+let z = 42
+console.log(x + y + z);
+
+// TASK 2
+const sec = 60
+const min = 60
+const hours = 24
+const days = 365
+const myAgeInSeconds = console.log(20 * days * hours * min * sec);
+
+// TASK 3
+let count = 42
+let userName = '42'
+let contString = String(cont)
+let userNameNumber = Number(userName)
+console.log(typeof contString, userNameNumber);
+
+// TASK 4
+let a = "1"
+let b = "2"
+let c = "белых медведей"
+let ABC = a + b + " " + c
+console.log(ABC);
+
+// TASK 5
+let a = "доступ"
+let b = "морпех"
+let c = "наледь"
+let d = "попрек"
+let e = "рубило"
+let ABCDE = a + b + c + d + e;
+console.log(ABCDE.length);
+
+// TASK 6
+
+// TASK 7
+let name = prompt("Как вас зовут:")
+let age = prompt("Укажите свой возраст:")
+console.log('Мой возраст: ', myAge, 'Мое имя:',myName);
