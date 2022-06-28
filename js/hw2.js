d2a99e904e7ae8e600b148e67b202a5e7d6ed2df
dmitriynsv <tatyana@MacBook-Pro-Tatyana.local>
2022-06-28 22:02:32 +0300

    added hw2.js

--- js/hw2.js
+++ js/hw2.js
@@ -1,8 +1,8 @@
 // task 1
-let x = 20;
-let y = 58;
-let z = 42;
-let sum = x+y+z;
+const x = 20;
+const y = 58;
+const z = 42;
+const sum = x+y+z;
 console.log(sum);
 // task 2
 const sek = 60;
@@ -13,35 +13,35 @@ const age = 45;
 const myAgeInSeconds = sek*min*hour*day*age;
 console.log(myAgeInSeconds);
 // task 3
-let count = 42
-let userName = '42'
-// let count_userName = count + userName;
-// let count_userName = count + +userName;
-let count_userName = Number(userName) + count;
+const count = 42
+const userName = '42'
+// const count_userName = count + userName;
+// const count_userName = count + +userName;
+const count_userName = Number(userName) + count;
 
 console.log(count_userName);
  
 // task 4
-let a = 1
-let b = 2
-let c = "белых медведей"
-let  abc  = a + (b + c);
+const a = 1
+const b = 2
+const c = "белых медведей"
+const  abc  = a + (b + c);
 console.log(abc);
 
 // task5
-let d = 'доступ'; 
-let m = 'морпех';
-let n = 'наледь';
-let p = 'попрек';
-let r = 'рубило';
-let all = d+m+n+p+r;
+const d = 'доступ'; 
+const m = 'морпех';
+const n = 'наледь';
+const p = 'попрек';
+const r = 'рубило';
+const all = d+m+n+p+r;
 
 var str = all;
 console.log(str.length);
 
 // task 6
- const variable = 'Variable: %variable name%';
- let type = 'have type: %type variable%';
+ const variable = 'Variable: variable name';
+ let type = 'have type: type variable';
  console.log (variable,type);
 
 
@@ -51,6 +51,7 @@ console.log(str.length);
   console.log( name, years);
 
 // advans level, task 1
+
 let s = 4
 let e = 3
 console.log(s,e);
