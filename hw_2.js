fc6abf929cd858267a636baea5d49ac61ff90242
Dasha Yaskevich <dyaskev@gmail.com>
2022-06-29 18:29:54 +0300

    index.html

---
+++ hw_2.js
@@ -0,0 +1,36 @@
+let x = 20;
+let y = 58;
+let z = 42; 
+
+console.log(`${x+y+z}`);
+
+let sInMin = 60;
+let minInH = 60;
+let hInD = 24;
+let dInY = 365;
+let myAgeInSeconds = 662256000n;
+
+let count = 42;
+let UserName = count;
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+let a_str = String (a);
+let b_str = String (b);
+console.log(a_str + b_str + c);
+
+let w1 = Number ("доступ");
+let w2 = Number ("морпех");
+let w3 = Number ("наледь");
+let w4 = Number ("попрек");
+let w5 = Number ("рубило");
+let lengthWords = w1 + w2 + w3 + w4 + w5;
+console.log(lengthWords);
+
+
+let age = prompt ('Сколько тебе лет?');
+alert (age);
+let name = prompt ('Как тебя зовут?');
+alert (name);
+console.log(age, name);
