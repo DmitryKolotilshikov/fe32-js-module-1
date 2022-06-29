50df4d3b6cb56286c61afef172482dc76d249216
Darya <104902569+lirikochi@users.noreply.github.com>
2022-06-29 19:24:52 +0300

    Update hw_2.js

--- hw_2.js
+++ hw_2.js
@@ -1,18 +1,21 @@
+// Задание 1
 let x = 20;
 let y = 58;
 let z = 42; 
-
 console.log(`${x+y+z}`);
 
+//Задание 2
 let sInMin = 60;
 let minInH = 60;
 let hInD = 24;
 let dInY = 365;
 let myAgeInSeconds = 662256000n;
 
+//Задание 3
 let count = 42;
 let UserName = count;
 
+//Задание 4
 let a = 1;
 let b = 2;
 let c = "белых медведей";
@@ -20,17 +23,24 @@ let a_str = String (a);
 let b_str = String (b);
 console.log(a_str + b_str + c);
 
-let w1 = Number ("доступ");
-let w2 = Number ("морпех");
-let w3 = Number ("наледь");
-let w4 = Number ("попрек");
-let w5 = Number ("рубило");
-let lengthWords = w1 + w2 + w3 + w4 + w5;
-console.log(lengthWords);
+//Задание 5
+let w1 = 'доступ';
+let w2 = 'морпех';
+let w3 = 'наледь';
+let w4 = 'попрек';
+let w5 = 'рубило';
+const allWords = w1+w2+w3+w4+w5;
+console.log(allWords.length);
 
+//Задание 6
+const animal = 'cat';
+let year = 64;
+let ageChecked = true;
+console.log(`Variable: ${animal} have type: String; Variable: ${year} have type: Number; Variable: ${ageChecked} have type: Boolean`);
 
+//Задание 7
 let age = prompt ('Сколько тебе лет?');
 alert (age);
-let name = prompt ('Как тебя зовут?');
-alert (name);
-console.log(age, name);
+let userName = prompt ('Как тебя зовут?');
+alert (userName);
+console.log(age, userName);
