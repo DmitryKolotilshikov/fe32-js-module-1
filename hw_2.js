d511bfdad61e7548ede51e7a3f3621d203c06455
Victoria <102157459+Vikamnv@users.noreply.github.com>
2022-07-01 10:28:51 +0300

    init(hw_2):my second HW (#25)
    * init(hw_2):mu second HW
    
    * Update hw_2.js

---
+++ hw_2.js
@@ -0,0 +1,55 @@
+console.log("Task 1");
+const x = 20;
+const y = 58;
+const z = 42;
+const sum = x+y+z;
+console.log(sum);
+
+console.log("Task 2");
+const sec =60;
+const min = 60;
+const hours = 24;
+const days = 365;
+myAgeInSeconds = 22*days*hours*min*sec;
+console.log(myAgeInSeconds);
+
+console.log("Task 3");
+const count = 42;
+const userName = '42';
+const strcount = String(count);
+const str1count = '${count}';
+console.log(typeof strcount);
+console.log(typeof str1count);
+const numUsername = Number(userName);
+const numUsername1 = +userName;
+console.log(typeof numUsername);
+console.log(typeof numUsername1);
+
+console.log("Task 4");
+const a = 1
+const b = 2
+const c = "белых медведей"
+const abc = String(a) + String(b) + " " + c; 
+console.log(abc);
+
+console.log("Task 5");
+const first = "доступ"; 
+const second = "морпех";
+const third = "наледь";
+const fourth = "попрек";
+const fifth = "рубило";
+const lengthWords = fifth + fourth + third + second + first;
+console.log(lengthWords.length);
+
+console.log("Task 6");
+const variable1 = "string";
+const variable2 = 6;
+const variable3 = true;
+console.log("Variable1:", variable1, "have type:", typeof variable1);
+console.log("Variable2:", variable2, "have type:", typeof variable2);
+console.log("Variable3:", variable3, "have type:", typeof variable3);
+console.log("Task 7");
+const myAge = prompt('Enter your age');
+const myName = prompt('Enter your name');
+console.log('Мой возраст: ', myAge, 'Мое имя:',myName);
+
