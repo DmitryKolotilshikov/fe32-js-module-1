bd0cb844645d4d9add7a212e504106e82ead2404
filinator <ioannvaletov@gmail.com>
2022-07-05 22:49:01 +0300

    added hw2

---
+++ hw2/ivan-hw2.js
@@ -0,0 +1,133 @@
+'use strict';
+
+// Task 1
+// Создайте переменные, затем сложите их и выведите результат в консоль разработчика.
+
+// const x = 20;
+// const y = 58;
+// const z = 42;
+
+// const sum = x + y + z;
+
+// console.log(sum);
+
+
+// Task 2
+// Создайте переменные:
+
+// - количество секунд в минуте
+// - количество минут в часу
+// - количество часов в сутках
+// - количество суток в году
+
+// Посчитайте ваш возраст в секундах и поместите результат в переменную myAgeInSeconds
+
+// const secInMin = 60;
+// const minInHour = 60;
+// const hourInDay = 24;
+// const daysInYear = 365;
+// const myAgeInYear = 25;
+
+// const myAgeInSeconds = secInMin * minInHour * hourInDay * daysInYear * myAgeInYear;
+
+// console.log (myAgeInSeconds);
+
+
+// Task 3
+// Создайте две переменные. Поместите в них переменную count и превратите в строку, а userName наоборот в число.
+// Попробуйте реализовать задачу двумя разными способами.
+
+// const count = 42
+// const userName = '42'
+
+// const strCount = String(count);
+// const numUserName = Number(userName);
+
+// console.log(typeof strCount, typeof numUserName);
+
+
+// Task 4
+// Имеется три переменныe
+// Сложите переменные так, чтобы в результате получилось выражение: 12 белых медведей и результат выведите в консоль.
+
+// const a = 1
+// const b = 2
+// const c = "белых медведей"
+
+// const strA = String(a);
+// const strB = String(b);
+
+// console.log(strA + strB, c);
+
+
+// Task 5
+// Создайте переменные и поместите в них нижеприведенные слова. 
+// Затем создайте еще одну переменную lengthWords и посчитайте в ней длинну всех слов из списка:
+
+// const a = 'доступ';
+// const b = 'морпех';
+// const c = 'наледь';
+// const d = 'попрек';
+// const e = 'рубило';
+
+// const lengthWords = a.length + b.length + c.length + d.length + e.length;
+
+// console.log(lengthWords);
+
+
+// Task 6
+// Создать 3 переменные разных типов и вывести в консоль для каждой из них строку следующего вида: `Variable: %variable name% have type: %type variable%`
+
+// const bool = false;
+// const str = "дерево";
+// const num = 10;
+
+// console.log("Variable: ", bool, "have type: ", typeof bool);
+// console.log("Variable: ", str, "have type: ", typeof str);
+// console.log("Variable: ", num, "have type: ", typeof num);
+
+
+// Task 7
+// Запросить у пользователя имя и возраст и вывести их в консоль.
+
+// const userName = prompt('Enter your name');
+// alert (userName);
+
+// const userAge = prompt('Enter your age');
+// alert (userAge);
+
+// console.log(userName, userAge);
+
+
+
+// ADVANCED level
+
+// Task 1
+// Поменяйте значение переменных местами не создавая дополнительной переменной:
+
+// let a = 4;
+// let b = 3;
+
+// --a;
+// ++b;
+
+// console.log(a, b);
+
+
+// Task 2
+// В данном послании скрыт шифр! Чтобы его разгадать нам нужно соединить все вторые символы слов!
+// Создайте переменную cipher и поместите туда необходимые символы
+
+// const codeWord1 = "обернись";
+// const codeWord2 = "неужели";
+// const codeWord3 = "огурцы";
+// const codeWord4 = "липкие";
+// const codeWord5 = "?!";
+
+// const cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
+
+// console.log(cipher);
+
+
+
+
