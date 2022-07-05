a5294cf724575a4443a08d18afea6ef373d20aa4
Dasha Yaskevich <dyaskev@gmail.com>
2022-07-05 21:48:15 +0300

    hw_3.js

--- hw_3.js
+++ hw_3.js
@@ -20,19 +20,48 @@ for (let i = 0; i <= 20; ++i){
     if (i%3 === 0) {console.log(i);}
 }
 
-
-
+//ex.4
 const key = true;
-    const documents = true;
-    const pen = true;
-    const apple = false;
-    const orange = true;
+const documents = true;
+const pen = true;
+const apple = false;
+const orange = true;
+const shouldGoToWork = (key && documents && pen && (apple||orange));
+console.log(shouldGoToWork);
 
-    if (key && documents && pen && (apple || orange)) {
-        console.log(shouldGoToWork);
-      } else {
-        console.log(stayHome);
-      }
+//ex.5
+const enterNumber = prompt ('Введите число', '');
+if (enterNumber%5==0) {
+    console.log('Fiz');
+} else if (enterNumber%3==0) {
+    console.log('Buz');
+} else if (enterNumber%5==0 && enterNumber%3==0){
+    console.log('FizBuz');
+};
 
+//ex.6
+const enterAge = prompt ('Сколько тебе лет?', '');
+if (enterAge>18) {
+    alert ("Попей пивка")
+} else if (enterAge<18) {
+    alert ("Пей колу")
+} if (enterAge <= 18 && enterAge>=16){
+    alert ('Можешь выкурить сигаретку, только маме не говори.')
+};
 
-
+//ex.7
+const world = prompt ('В какую сторону света планируете отправиться?', '');
+switch (world) {
+    case 'юг':
+        alert ('на юг пойдешь счастье найдешь');
+        break;
+    case 'север':
+        alert ('на север пойдешь много денег найдешь');
+        break;
+    case 'запад':
+        alert ('на запад пойдешь верного друга найдешь');
+        break;
+    case 'восток':
+        alert ('на восток пойдешь разработчиком станешь');
+        break;
+}
