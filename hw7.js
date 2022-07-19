157cc0da1d6f3aeff0b03fcc30e644d07771d453
Shovgur <s.showgur@yandex.by>
2022-07-19 18:57:50 +0300

    Added hw 7

--- hw7.js
+++ hw7.js
@@ -1,5 +1,6 @@
 // Task 1
 
+//Выведи все элементы массива в консоль с помощью метода forEach
 
 const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
 
@@ -8,6 +9,9 @@ fibonacci.forEach(element => console.log(element))
 
 // Task 2 
 
+//Используя метод map создайте новый массив, на основе массива users, в котором каждый элемент массива будет содержать строку вида:
+
+//['member 1: Darya', 'member 2: Masha', ... etc]
 
 const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton']
 
@@ -18,6 +22,7 @@ console.log(users1)
 
 // Task 3 
 
+//С помощью метода filter создайте новый массив в котором не будет отрицательных чисел.
 
 const numbers = [7, -4, 32, -90, 54, 32, -21]
 
@@ -29,6 +34,7 @@ console.log(positiveNumbers)
 
 // Task 4 
 
+//Используя метод reduce получите сумму всех чисел массива.
 
 const fibonacci1 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
 
@@ -41,6 +47,7 @@ console.log(result)
 
 // Task 5 
 
+//Используя метод find найдите в массиве первое четное число.
 
 const numbers1 = [5, 9, 13, 24, 54, 10, 13, 99, 1, 5]
 
