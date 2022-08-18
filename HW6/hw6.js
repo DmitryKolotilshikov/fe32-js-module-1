b37bea991ab3c54ab742b788a4e3f521520d14c3
Tim143 <57316731+Tim143@users.noreply.github.com>
2022-08-18 11:29:18 +0300

    fixed hw issues

--- HW6/hw6.js
+++ HW6/hw6.js
@@ -1,10 +1,13 @@
 //HW-6
 //Task-1
+//Дан массив:
 const colors = ['red', 'green', 'blue'];
-
+//Выведите в консоль его длину.
 console.log(colors.length);
 
 //Task-2
+//Дан массив:
+//Выведите в консоль его последний элемент вне зависимости от его длинны.
 const animals = ['monkey', 'dog', 'cat'];
 
 const lastElement = animals[animals.length - 1];
@@ -16,6 +19,8 @@ console.log(reverseArr[0]);
 console.log(lastElement2);
 
 //Task-3
+//Дан массив:
+//Удалите все элементы в массиве и выведите в консоль полученный результат.
 const numbers = [5, 43, 63, 23, 90];
 console.log(numbers);
 
@@ -30,6 +35,10 @@ while(numbers.length !== 0){
 }
 
 //Task-4
+//Дан массив:
+// Удалите последний элемент массива, затем вместо него добавьте в массив студента Borya.
+// Удалите первый элемент массива, затем вместо него добавьте в массив студента Andrey.
+// Полученный результат не забудьте вывести в консоль.
 const students = ['Polina', 'Dasha', 'Masha'];
 
 students.splice(students.length - 1, 1, "Borya");
@@ -38,6 +47,8 @@ students.splice(students[0], 1, "Andrey");
 console.log(students);
 
 //Task-5
+//Дан массив:
+//Выведите в консоль все элементы массива. Сначала через цикл for, затем for of.
 const cats = ['Gachito', 'Tom', 'Batman'];
 
 for(let i = 0; i < cats.length; i++){
@@ -49,6 +60,8 @@ for (const index of cats) {
 }
 
 //Task-6
+// Соедините два массива чисел в один.
+// В полученном массиве попробуйте найти индекс числа 8
 const evenNumbers = [2, 4, 6, 8, 10];
 const oddNumbers = [1, 3, 5, 7, 9];
 
@@ -57,6 +70,8 @@ const finalArr = evenNumbers.concat(oddNumbers);
 console.log(finalArr);
 
 //Task-7
+// Наш бинарный массив неполный, в нем явно не хватает единиц.
+// Превратите данный массив в строку.
 const binary = [0, 0, 0, 0];
 
 for (const iterator of binary) {
@@ -67,6 +82,7 @@ console.log(binary);
 
 //Advanced
 //Task-1
+// Реализуйте функцию которая будет проверять, является ли слово палиндромом.
 
 const checkFroPolindrom = (word) => {
     let regex = /[^A-Za-z0-9]/g;
@@ -85,6 +101,7 @@ const checkFroPolindrom = (word) => {
 console.log(checkFroPolindrom("logol"));
 
 //Task-2
+//Выведите в консоль среднее значение чисел в многомерном массиве.
 const matrix = [
     [12, 98, 78, 65, 23],
     [54, 76, 98, 43, 65],
@@ -105,6 +122,7 @@ for (let i = 0; i < matrix.length; i++){
 console.log(matrixSum / totalElements);
 
 //Task-3
+//Создайте два массива, в один поместите все положительные числа включая 0, в другой все отрицательные. Оба массива затем выведите в консоль.
 const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4];
 
 const positiveArr = [];
@@ -123,6 +141,9 @@ console.log(positiveArr);
 console.log(negativeArr);
 
 //Task-4
+// Создать массив длинной не менее 5, из динамически созданных случайных чисел.
+// Далее написать алгоритм который берет все числа из исходного массива,
+// возводит их в куб и записывает в новый массив. В конце вывести оба массива в консоль.
 const generateArr = (arrLenth) => {
     const arr = [];
 
